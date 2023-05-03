fd276b71a3fb5c6a0a57ad08ce85bb9a1cfc7677
hyunin3 <stock4share2@gmail.com>
2023-05-03 12:33:08 +0900

    0503

---
+++ vue-random/src/components/MyLotto.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>이번주의 행운번호</h1>
+    <button @click="getLottoNumber">행운번호 받기</button>
+    <hr>
+    <p v-if="lottoNumber.length">{{ lottoNumber }}</p>
+  </div>
+</template>
+
+<script>
+import lodash from 'lodash'
+
+export default {
+  name: 'MyLotto',
+  data () {
+    return {
+      lottoNumber: [],
+    }
+  },
+  methods: {
+    getLottoNumber () {
+      const numbers = lodash.range(1, 46)
+      this.lottoNumber = lodash.sampleSize(numbers, 6)
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-random/src/components/MyLunch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>점심 메뉴 추천</h1>
+    <button @click="getLunch">오늘은 뭘 먹으면 좋을까</button>
+    <hr>
+    <p v-if="lunch">오늘의 추천 메뉴는 {{ lunch }}입니다!</p>
+  </div>
+</template>
+
+<script>
+import lodash from 'lodash'
+
+export default {
+  name: 'MyLunch',
+  data () {
+    return {
+      lunchMenu: ['짜장면', '삼겹살', '초밥', '돈까스', '비빔밥'],
+      lunch: ''
+    }
+  },
+  methods: {
+    getLunch () {
+      this.lunch = lodash.sample(this.lunchMenu)
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
