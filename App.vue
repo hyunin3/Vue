dae410fc77a073bc0a41a8d7ea483e80e27f944d
hyunin3 <stock4share2@gmail.com>
2023-05-04 17:49:33 +0900

    0504

---
+++ App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <input type="text" v-model="appData" @input="inputData">
+   <AppChild 
+    :my-props="dynamicProps"
+    :app-data="appData"
+
+   />
+  </div>
+</template>
+
+<script>
+import AppChild from './components/AppChild.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppChild
+  },
+  data(){
+    return {
+      appData : ""
+    }
+  },
+  methods:{
+    inputData(){
+      this.$emit('app-to-home',this.appData )
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
